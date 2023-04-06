ce756df22bfeaf5ad95d0a545185c31535efbe72
Ridennator <rlcpm@hotmail.com>
2023-04-06 20:58:56 +0100

    Progresso na pagina home

--- views/Home.js
+++ views/Home.js
@@ -1,46 +1,17 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ImageBackground } from 'react-native';
 
 export default function HomeScreen({ navigation }) {
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text style={{ fontSize: 26, fontWeight: 'bold' }}>
-                Home Screen</Text>
-            <View style = {{Home}}>
-                <image 
-                    style={home.img}
-                    source = {require = {}}/>
-                <Text style = {{fontSize: 26, fontFamily: "Fira Sans"}}>
-                    
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', width:'100%', height:'100%', }}>
+            <ImageBackground 
+             style={{height:'100%', width:'100%'}}
+             resizeMode='cover'
+             source={require('../data/img/views/home/abrantes2.png')}>
+                <Text style = {{fontSize: 26, fontFamily: "Fira Sans", }}>
+                    Teste
                 </Text>
-            </View>
-            <View style = {{Home}}>
-                <image 
-                    style={home.img}
-                    source = {require = {}}/>
-                <Text></Text>
-            </View>
-                <image 
-                    style={home.img}
-                    source = {require = {}}/>
-                <Text></Text>
-            <View style = {{Home}}>
-                <image 
-                    style={home.img}
-                    source = {require = {}}/>
-                <Text></Text>
-            </View>
-
-            <View style = {{Home}}>
-                <image 
-                    style={home.img}
-                    source = {require = {}}/>
-                <Text></Text>
-            </View>
-
-            <View style = {{NavBar}}>
-
-            </View>
+            </ImageBackground>
         </View>
         
     );
