95f1bbaeb0a12e137060b1d785e6970101fc10f3
Ana Patrícia <78435927+AnaPatriciaGraca@users.noreply.github.com>
2023-04-13 00:26:50 +0100

    SplashScreen working

--- App.tsx
+++ App.tsx
@@ -1,10 +1,29 @@
-import * as React from 'react';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, StyleSheet, View } from 'react-native';
 import NavBar from './views/NavBar';
+import SplashScreen from './SplashScreen';
 
-function App() {
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate a delay to show the splash screen for a few seconds
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 500);
+  }, []);
+
   return (
-    <NavBar/>
+    <View style={styles.container}>
+      {isLoading ? <SplashScreen /> : <NavBar />}
+    </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default App;
