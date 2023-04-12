e53f24feb8ac051cc8c5de6a4a13f62bcc86f18c
Ana Patrícia <78435927+AnaPatriciaGraca@users.noreply.github.com>
2023-04-12 20:52:21 +0100

    Revert "Merge branch 'main' of https://github.com/iptomar/RaulLinoF"
    This reverts commit d5152d1131c91f0fb04f301451aba04e69ddce2c, reversing
    changes made to 761fb55c08fddcc4ae23e1ceb089e1b89c47d98c.

--- views/Home.js
+++ views/Home.js
@@ -1,21 +1,10 @@
 import * as React from 'react';
-import { View, Pressable, ImageBackground } from 'react-native';
+import { View, Text } from 'react-native';
 
 export default function Home() {
     return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <ImageBackground 
-             style={{height:'100%', width:'100%'}}
-             resizeMode='cover'
-             source={require('../data/img/views/home/abrantes2.png')}>
-                {/*
-                <Pressable 
-                    onPress= ""
-                    title = "Explorar"
-                    style = {{fontSize: 26, fontFamily: "Fira Sans", position: 'absolute', left: '40%', top:'50%', opacity: 100 }}>
-                </Pressable>
-                */}
-            </ImageBackground>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Text style={{color:"red"}}>Home</Text>
     </View>
     );
 }
