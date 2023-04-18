a4fb10ab6f88905386b745cec071cbdfe384f917
Ridennator <rlcpm@hotmail.com>
2023-04-18 18:21:33 +0100

    Major progress
    Missing: Icons on buttons

--- views/ar/AR.js
+++ views/ar/AR.js
@@ -1,11 +1,90 @@
 import * as React from 'react';
-import { View, Pressable, ImageBackground, Text } from 'react-native';
+import { View, Pressable, ImageBackground, Text, Image, StyleSheet, Button } from 'react-native';
+
+
+import Details from '../../data/img/views/ar/detailsIcon.svg';
+
+const style = StyleSheet.create({
+    
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+    header: {
+        paddingTop: 20,
+        borderBottomColor: '#FFD683',
+        borderBottomWidth: 5,
+    },
+    subtitle: {
+        fontSize: 20,
+        fontFamily: 'Unbounded-Regular',
+        color: 'black'
+    },
+    description: {
+        maxWidth: '100%',
+        fontSize: 16,
+        textAlign: 'left',
+        fontFamily: 'FiraSans-Regular',
+        color: '#272133',
+    },
+    image: {
+        width: '80%',
+        height: '100%',
+        marginBottom: 5,
+    },
+    teste:{
+        flexDirection: 'row',
+        height : '50%',
+        width: '100%'
+    },
+
+    button1:{
+        maxWidth: '50%',
+        maxHeight: '50%',
+        left: '50%',
+        height: '100%',
+        backgroundColor: "#FFD683"
+    },
+    
+    button2:{
+        top: '0%',
+        maxWidth: '50%',
+        maxHeight: '50%',
+        left: '50%',
+        height: '100%',
+        backgroundColor: "#FFD683"
+    },
+    buttonContainer:{
+        marginTop:'15%',
+        width:'20%',
+        height: '50%',
+    },
+
+
+})
 
 export default function HomeScreen({ navigation }) {
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', width:'100%', height:'100%', }}>
-            <Text>
-                Assemblei de Abrantes 
+        <View style={style.container}>
+            <Text style= {style.header}>
+                Realidade Aumentada
+            </Text>
+            <Text style= {style.subtitle}>
+                Assembleia de Abrantes 
+            </Text>
+            <View style ={style.teste}>
+                <Image 
+                    style ={style.image}
+                    source={require("../../data/img/views/ar/abrantes.jpg")}/>
+                <View style = {style.buttonContainer}>
+                    <Pressable style= {style.button1}><Text>teste</Text></Pressable>
+                    <Pressable style= {style.button2}><Text>teste</Text></Pressable>
+                </View>
+            </View>
+            <Text style={style.description}>
+                A fun­dação de Abrantes ter-se-á ve­ri­fi­cado em me­ados do sé­culo XII, re­sul­tado da ne­ces­si­dade de de­fesa dos ter­ri­tó­rios con­quis­tados e de as­se­gurar a vida ativa de San­tarém. Para me­lhor as­se­gurar essa de­fesa, D. Afonso Hen­ri­ques doou o seu Cas­telo e ex­tenso termo à Ordem de S. Tiago da Es­pada em 1173 e seis anos de­pois con­cede-lhe foral (1179). As­so­ciada à origem do nome Abrantes existe uma lenda en­can­tada.
+                {"\n"}{"\n"}
+                O perfil de Abrantes é no sé­culo XII, ine­qui­vo­ca­mente mi­litar, con­di­ci­o­nando a im­plan­tação da es­tru­tura amu­ra­lhada e Cas­telo, num local di­fi­cil­mente ex­pug­nável e usando o leito do rio Tejo como obs­tá­culo na­tural.
             </Text>
         </View>
         
