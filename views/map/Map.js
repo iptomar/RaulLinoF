cf2d958a5d62719d3b4830945a68bb1f078a2ffa
aluno20230 <aluno20230@ipt.pt>
2023-04-26 22:41:10 +0100

    Conclusão cálculo entre pontos
    Implementação do cálculo entre dois pontos selecionados pelo utilizador, da lista de pontos disponíveis

--- views/map/Map.js
+++ views/map/Map.js
@@ -42,101 +42,108 @@ function getDistance(lat1, lon1, lat2, lon2) {
       Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
       (1 - Math.cos(dLon)) / 2;
   
-    return R * 2 * Math.asin(Math.sqrt(a)); // Distance in km
-  }
+    const distanceInKm = R * 2 * Math.asin(Math.sqrt(a)); // Distance in km
+    const distanceInMeters = distanceInKm * 1000; // Distance in meters
+
+    return distanceInMeters;
+}
 
 
-
-  export default function Map({ navigation }) {
+export default function Map({ navigation }) {
     const [distance, setDistance] = React.useState(null);
+    const [point1, setPoint1] = React.useState(null);
+    const [point2, setPoint2] = React.useState(null);
     const [markers, setMarkers] = React.useState([
         {
-            title: "Marker Vermelho",
-            description: "Marker Vermelho",
+            title: "Ponto A",
+            description: "A",
             coordinate: {
                 latitude: 39.461152, 
                 longitude: -8.189
             },
-            pinColor: "red"
+            pinColor: "red",
+            isSelected: false
         },
         {
-            title: "Marker Azul",
-            description: "Marker Azul",
+            title: "Ponto B",
+            description: "B",
             coordinate: {
                 latitude: 39.4705,
                 longitude: -8.2099
             },
-            pinColor: "blue"
+            pinColor: "blue",
+            isSelected: false
+        },
+        {
+            title: "Ponto C",
+            description: "C",
+            coordinate: {
+                latitude: 39.4905,
+                longitude: -8.2099
+            },
+            pinColor: "blue",
+            isSelected: false
+        }
+        ,
+        {
+            title: "Ponto D",
+            description: "D",
+            coordinate: {
+                latitude: 39.4805,
+                longitude: -8.2099
+            },
+            pinColor: "blue",
+            isSelected: false
         }
     ]);
 
-    React.useEffect(() => {
-        if (markers.length >= 2) {
-            const lat1 = markers[0].coordinate.latitude;
-            const long1 = markers[0].coordinate.longitude;
-            const lat2 = markers[1].coordinate.latitude;
-            const long2 = markers[1].coordinate.longitude;
-            const distance = Math.sqrt((lat2 - lat1) ** 2 + (long2 - long1) ** 2);
-            setDistance(distance);
-        }
-    }, [markers]);
+const handleMarkerPress = (index) => {
+  const newMarkers = [...markers];
+  newMarkers[index].isSelected = !newMarkers[index].isSelected;
+  setMarkers(newMarkers);
+};
 
-    return (
-        <View style={{flex: 1}}>
-            <MapView
-                style={{flex: 1}}
-                initialRegion={getInitialState()}>
-                {markers.map((marker, index) => (
-                    <Marker
-                        key={index}
-                        coordinate={marker.coordinate}
-                        title={marker.title}
-                        description={marker.description}
-                        pinColor={marker.pinColor}
-                    />
-                ))}
-            </MapView>
-            {distance !== null && (
-                <View style={{position: 'absolute', bottom: 0, left: 0, right: 0, backgroundColor: 'white', padding: 10}}>
-                    <Text>Distance: {distance}</Text>
-                </View>
-            )}
-        </View>
-    );
+
+React.useEffect(() => {
+    const selectedMarkers = markers.filter((marker) => marker.isSelected);
+    if (selectedMarkers.length >= 2) {
+        const lat1 = selectedMarkers[0].coordinate.latitude;
+        const long1 = selectedMarkers[0].coordinate.longitude;
+        const lat2 = selectedMarkers[1].coordinate.latitude;
+        const long2 = selectedMarkers[1].coordinate.longitude;   
+        const distance = getDistance(lat1, long1, lat2, long2);
+        setDistance(Number(distance.toFixed(0))); // Format the distance as a string with zero decimal places and convert it back to a number
+        setPoint1(selectedMarkers[0].title);
+        setPoint2(selectedMarkers[1].title);
+    } else {
+        setDistance(null);
+        setPoint1(null);
+        setPoint2(null);
+    }
+}, [markers]);
+
+return (
+    <View style={{flex: 1}}>
+        <MapView
+            style={{flex: 1}}
+            initialRegion={getInitialState()}>
+            {markers.map((marker, index) => (
+                <Marker
+                    key={index}
+                    coordinate={marker.coordinate}
+                    title={marker.title}
+                    description={marker.description}
+                    pinColor={marker.isSelected ? "green" : marker.pinColor}
+                    onPress={() => handleMarkerPress(index)}
+                />
+            ))}
+        </MapView>
+        {distance !== null && (
+            <View style={{position: 'absolute', bottom: 0, left: 0, right: 0, backgroundColor: 'white', padding: 10}}>
+                <Text>Distância entre {point1} e  {point2}: {distance} metros</Text>
+            </View>
+        )}
+    </View>
+);
+
 }
-
-
-
-
-
-
-
-
-
-
-//export default function Map({ navigation }) {
- //   return (
-  //      <MapView
- //           style={{height:"100%"}}
-  //          initialRegion={getInitialState()}>              
- ///           <Marker
-   //             coordinate={{
- //                   latitude: 39.461152, 
- //                   longitude: -8.189
- //               }} 
-  //              title="Marker Vermelho"
-  //              description="Marker Vermelho"
-  //              pinColor="Red"              
-  //          />
- //           <Marker
-  //           coordinate={{
-  //                  latitude: 39.4705,
-   //                 longitude: -8.2099
- //               }}
- //               title="Marker Azul"
- //               description="Marker Azul"
- //               pinColor="blue"
-  //          />
- //       </MapView>
- //   );
-//}
