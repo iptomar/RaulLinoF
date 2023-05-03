475a24f5f78ffa030ae556c7f24b93a548633f20
Ana Patrícia <78435927+AnaPatriciaGraca@users.noreply.github.com>
2023-05-03 17:16:10 +0100

    on Screen Buttons

--- views/map/Map.js
+++ views/map/Map.js
@@ -1,12 +1,31 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet} from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 import MapView, { Marker, Callout } from '@mvits/react-native-maps-osmdroid';
 import itinerarios from '../itinerarios';
 import Geolocation from 'react-native-geolocation-service';
 import MarkersIt from './MarkersIt';
-import MarkerIconYellow from '../../data/img/views/mapa/selectedMarker.svg';
 import MarkerMyLocal from '../../data/img/views/mapa/currentPositionCircle.svg';
+import MapItinerary from '../../data/img/views/mapa/itinerarioIcon.svg';
+import MapHistory  from '../../data/img/views/mapa/historiaOnClickIcon.svg';
 
+const styles = StyleSheet.create({
+    button: {
+        position: 'absolute',
+        top: 20,
+        right: 0,
+        backgroundColor: '#272133',
+        padding: 10,
+        borderRadius: 5,
+    },
+    button2: {
+        position: 'absolute',
+        top: 80,
+        right: 0,
+        backgroundColor: '#272133',
+        padding: 10,
+        borderRadius: 5,
+    },
+});
 
 // Returns the initial map state (Abrantes)
 function getInitialState() {
@@ -42,8 +61,14 @@ function numItinerarios(){
        console.log(location.coords.longitude, location.coords.latitude);
 
  }
-export default function Map({ navigation }) {
+
+ export default function Map({ navigation }) {
     const [currentLocation, setCurrentLocation] = useState(false);
+
+    //for the button on the top right corner
+    useEffect(() => {
+        //...
+      }, []);
 
     //get the current location of the phone
     useEffect(() => {
@@ -61,21 +86,34 @@ export default function Map({ navigation }) {
         );
     }, []);
 
+
     return (
-        
-        <MapView
-            style={{flex:1}}
-            initialRegion={getInitialState()}>
-            {currentLocation && (
-                <Marker 
-                    coordinate={currentLocation} 
-                    title="My Location"
-                    >
-                    <MarkerMyLocal width="50" height="50"/>
-                </Marker>
-            )}
-            {/* Markers from the "itenerarios.js" file */}
-            <MarkersIt />
-        </MapView>
+        <View style={{ flex: 1 }}>
+            <MapView
+                style={{flex:1}}
+                initialRegion={getInitialState()}>
+                {currentLocation && (
+                    <Marker 
+                        coordinate={currentLocation} 
+                        title="My Location"
+                        >
+                        <MarkerMyLocal width="50" height="50"/>
+                    </Marker>
+                )}
+                {/* Markers from the "itenerarios.js" file */}
+                <MarkersIt />
+            </MapView>
+            {/* on screen buttons */}
+            <TouchableOpacity style={styles.button}
+                onPress={() => console.log('Button pressed')}
+                >
+                <MapItinerary width="50" height="50" />
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button2}
+                onPress={() => console.log('Button pressed')}
+                >
+                <MapHistory width="50" height="50" />
+            </TouchableOpacity>
+        </View>
     );
 }
--- views/map/MarkersIt.js
+++ views/map/MarkersIt.js
@@ -1,19 +1,19 @@
 import MapView, { Marker, Callout } from '@mvits/react-native-maps-osmdroid';
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, Image, ScrollView } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import itinerarios from '../itinerarios';
 import MarkerIcon from '../../data/img/views/mapa/marker.svg';
 import MarkerIconYellow from '../../data/img/views/mapa/selectedMarker.svg';
+import MapItinerary from '../../data/img/views/mapa/itinerarioIcon.svg';
 
-
-
-export default function MarkersIt(){
+export default function MarkersIt({}){
     const [markerStates, setMarkerStates] = useState({});
   
     //handle the Press on he Marker
     const handleMarkerPress = (itemId) => {
         setMarkerStates((prevState) => ({ ...prevState, [itemId]: !prevState[itemId] }));
     };
+
   
     return(
         <>
