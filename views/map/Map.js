6a02378c5b00955401d7b671a7dcec530cbd3069
Ana Patrícia <78435927+AnaPatriciaGraca@users.noreply.github.com>
2023-05-04 17:13:03 +0100

    Managing errors getting the location

--- views/map/Map.js
+++ views/map/Map.js
@@ -1,13 +1,18 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity} from 'react-native';
-import MapView, { Marker, Callout } from '@mvits/react-native-maps-osmdroid';
+import { View, Text, StyleSheet, TouchableOpacity, ActivityIndicator} from 'react-native';
 import itinerarios from '../Itinerarios';
-import MarkerIcon from '../../data/img/views/mapa/marker.svg';
+//markers
+import MarkerMyLocal from '../../data/img/views/mapa/currentPositionCircle.svg';
 import MapItinerary from '../../data/img/views/mapa/itinerarioIcon.svg';
 import MapHistory from '../../data/img/views/mapa/historiaOnClickIcon.svg';
-import { isReturnStatement } from 'typescript';
-import Geolocation from '@react-native-community/geolocation';
 import MarkersIt from './MarkersIt';
+import MapView, { Marker, Callout } from '@mvits/react-native-maps-osmdroid';
+//location stuff
+import { PermissionsAndroid, Platform } from 'react-native';
+import * as Permissions from 'react-native-permissions';
+import Geolocation from '@react-native-community/geolocation';
+
+
 
 const styles = StyleSheet.create({
     button: {
@@ -38,60 +43,74 @@ function getInitialState() {
     };
 }
 
-// Returns the number of buildings
-function numItinerarios(){
-    return itinerarios.dados.length;
-}
-
- /////   Joao Localiçao 
- const userLocation = async() =>{
-    let {status} = await location.requestForegroundPermissionsAsync();
-     if (status !== 'granted'){
-            Error('acesso negado');
-            return;
-
-   }
-   let location = await Location.getCurrentPositionAsync({enableHighAccuracy: true});
-        setMapRegion ({
-		latitude: location.coords.latitude,
-         longitude:location.coords.longitude,
-		 latitudeDelta: 0.0922,
-		 longitudeDelta: 0.0421,
-
-       });
-       console.log(location.coords.longitude, location.coords.latitude);
-
- }
-
  export default function Map({ navigation }) {
     const [currentLocation, setCurrentLocation] = useState(false);
 
-    //for the button on the top right corner
+    //for the buttons on the top right corner
     useEffect(() => {
         //...
       }, []);
 
-    //get the current location of the phone
+    //permissoons for the location - granted or not?
     useEffect(() => {
-        Geolocation.getCurrentPosition(
-        position => {
-            setCurrentLocation({
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude,
-            });
-        },
-        error => {
-            console.log(error.code, error.message);
-        },
-        { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 },
-        );
+        const checkLocationPermission = async () => {
+            if (Platform.OS === 'android') {
+                const granted = await PermissionsAndroid.request(
+                PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+                {
+                    title: 'Location Permission Required',
+                    message: 'This app requires access to your location.',
+                },
+                );
+                if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+                    console.log('Location permission granted');
+                } else {
+                    console.log('Location permission denied');
+                }
+            } else if (Platform.OS === 'ios') {
+                Geolocation.requestAuthorization();
+            }
+        };
+    
+        checkLocationPermission();
     }, []);
 
+    //get the current location of the phone
+    useEffect(() => {
+        const getCurrentLocation = async () => {
+            try {
+                const granted = await PermissionsAndroid.check(
+                    PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+                );
+                if (granted) {
+                    Geolocation.getCurrentPosition(
+                        position => {
+                            setCurrentLocation({
+                                latitude: position.coords.latitude,
+                                longitude: position.coords.longitude,
+                            });
+                            console.log('Current Location:', currentLocation);
+                        },
+                        error => {
+                            console.log('Error setting the location. Error:', error);
+                        },
+                        { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 },
+                );
+                } else {
+                console.log('Location permission denied');
+                }
+            } catch (err) {
+                console.warn(err);
+            }
+            };
+        
+            getCurrentLocation();
+        }, []);
 
     return (
-        <View style={{ flex: 1 }}>
+        <View style={{ height: '100%' }}>
             <MapView
-                style={{flex:1}}
+                style={{height: '100%'}}
                 initialRegion={getInitialState()}>
                 {currentLocation && (
                     <Marker 
@@ -118,3 +137,4 @@ function numItinerarios(){
         </View>
     );
 }
+ 
