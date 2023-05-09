9a37b89c068da6229c2d8290be9156ba3f5b65f6
Rúben David Costa Poupado <aluno20966@ipt.pt>
2023-05-09 17:44:02 +0100

    Added Submenu

--- views/ar/AR.js
+++ views/ar/AR.js
@@ -1,9 +1,10 @@
 import * as React from 'react';
-import { View, Pressable, Text, Image, StyleSheet } from 'react-native';
+import { View, TouchableOpacity, Text, Image, StyleSheet } from 'react-native';
 
+import DetailsIcon from '../../data/img/views/ar/detailsIcon.svg';
+import VideoIcon from '../../data/img/views/ar/videoIcon.svg';
 
 const style = StyleSheet.create({
-    
     container: {
         flex: 1,
         backgroundColor: '#fff',
@@ -35,33 +36,30 @@ const style = StyleSheet.create({
         height : '50%',
         width: '100%'
     },
-
-    button1:{
-        maxWidth: '50%',
-        maxHeight: '50%',
-        left: '50%',
-        height: '100%',
-        backgroundColor: "#FFD683"
-    },
-    
+    button: {
+        position: 'absolute',
+        top: 20,
+        right: 0,
+        backgroundColor: '#272133',
+        padding: 10,
+        borderRadius: 5,
+    }, 
     button2:{
-        top: '0%',
-        maxWidth: '50%',
-        maxHeight: '50%',
-        left: '50%',
-        height: '100%',
-        backgroundColor: "#FFD683"
+        position: 'absolute',
+        top: 80,
+        right: 0,
+        backgroundColor: '#272133',
+        padding: 10,
+        borderRadius: 5,
     },
     buttonContainer:{
         marginTop:'15%',
         width:'20%',
         height: '50%',
     },
-
-
 })
 
-export default function HomeScreen({ navigation }) {
+export default function AR({ navigation }) {
     return (
         <View style={style.container}>
             <Text style= {style.header}>
@@ -74,19 +72,20 @@ export default function HomeScreen({ navigation }) {
                 <Image 
                     style ={style.image}
                     source={require("../../data/img/views/ar/abrantes.jpg")}/>
-                {/*
-                <View style = {style.buttonContainer}>
-                    <Pressable style= {style.button1}><Image source={require("../../data/img/views/ar/detailsIcon.jpg")}/></Pressable>
-                    <Pressable style= {style.button2}></Pressable>
-                </View>
-    */}
             </View>
+
+            <TouchableOpacity style={style.button} onPress={() => console.log('Details pressed')}>
+                <DetailsIcon width="50" height="50" />
+            </TouchableOpacity>
+            <TouchableOpacity style={style.button2} onPress={() => console.log('Video pressed')}>
+                <VideoIcon width="50" height="50" />
+            </TouchableOpacity>
+
             <Text style={style.description}>
                 A fun­dação de Abrantes ter-se-á ve­ri­fi­cado em me­ados do sé­culo XII, re­sul­tado da ne­ces­si­dade de de­fesa dos ter­ri­tó­rios con­quis­tados e de as­se­gurar a vida ativa de San­tarém. Para me­lhor as­se­gurar essa de­fesa, D. Afonso Hen­ri­ques doou o seu Cas­telo e ex­tenso termo à Ordem de S. Tiago da Es­pada em 1173 e seis anos de­pois con­cede-lhe foral (1179). As­so­ciada à origem do nome Abrantes existe uma lenda en­can­tada.
                 {"\n"}{"\n"}
                 O perfil de Abrantes é no sé­culo XII, ine­qui­vo­ca­mente mi­litar, con­di­ci­o­nando a im­plan­tação da es­tru­tura amu­ra­lhada e Cas­telo, num local di­fi­cil­mente ex­pug­nável e usando o leito do rio Tejo como obs­tá­culo na­tural.
             </Text>
-        </View>
-        
+        </View>    
     );
 }
