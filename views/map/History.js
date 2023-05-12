8551c536c7fafb2ab633dd1dbaa83a9e77658cdd
Ana Patrícia <78435927+AnaPatriciaGraca@users.noreply.github.com>
2023-05-12 18:58:24 +0100

    css on History page

--- views/map/History.js
+++ views/map/History.js
@@ -1,24 +1,81 @@
 import * as React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, TouchableOpacity } from 'react-native';
 import itinerarios from '../Itinerarios';
+import MapHistory from '../../data/img/views/adicionar.svg';
+
+
+const styles = {
+    content: {
+        flexDirection: 'column',
+        backgroundColor: '#fff',
+    },
+    listPlaces:{
+        flexDirection:'row',
+        backgroundColor: '#272133',
+        justifyContent: 'space-between',
+    },
+    exapndBtn: {
+        backgroundColor: '#ead8c1',
+        alignSelf: 'flex-end',
+    },
+    title: {
+        fontSize: 18,
+        fontFamily: 'Unbounded-Regular',
+        color: 'black',
+        padding: 20,
+    },
+    subtitle: {
+        fontSize: 15,
+        fontFamily: 'Unbounded-Regular',
+        color: '#fff',
+        padding: 5,
+    },
+    subtitle2: {
+        fontSize: 15,
+        fontFamily: 'Unbounded-Regular',
+        color: '#000',
+        padding: 5,
+        backgroundColor: "#EAD8C1",
+    },
+    description: {
+        fontSize: 16,
+        fontFamily: 'FiraSans-Regular',
+        color: '#000',
+        padding: 5,
+        backgroundColor: '#EAD8C1',
+    }
+}
 
 export default function History({ navigation, route}) {
     const {selectedMarkersList} = route.params;
+    const [selectedItem, setSelectedItem] = React.useState(null);
     //verify each places to show
     const filteredItinerarios = itinerarios.filter(item =>
         selectedMarkersList.includes(item.id)
     );
 
+    const showDetails = (item) => {
+        setSelectedItem(item);
+      };
+
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text
-                onPress={() => alert('This is the "History" screen.')}
-                style={{ fontSize: 26, fontWeight: 'bold' }}>History Screen</Text>
+        <View style={styles.content}>
+            <Text style={styles.title}>MAPA INTERATIVO - HISTÓRIA</Text>
 
                 {filteredItinerarios.map(item => (
-                    <Text key={item.id}>{item.title}</Text>
+                    <View key={item.id} style={styles.listPlaces}>
+                        <Text style={styles.subtitle}>{item.year} - <Text style={styles.subtitle}>{item.title}</Text></Text>
+                        <TouchableOpacity>
+                            <MapHistory width="50" height="50" onPress={() => showDetails(item)}/>
+                        </TouchableOpacity>
+                    </View>
                 ))}
-
+            {selectedItem && (
+                <View style={styles.content}>
+                    <Text style={styles.subtitle2}>{selectedItem.title}</Text>
+                    <Text style={styles.description}>{selectedItem.info}</Text>
+                </View>
+            )}
             <Button title='Voltar' onPress={()=> navigation.navigate('Map')} color={'#078C6B'}/>
         </View>
     );
